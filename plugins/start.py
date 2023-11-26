2e2b41bcff21eb749e89a0861a20476c4aef7b1a
jaephefd12 <151659221+jaephefd12@users.noreply.github.com>
2023-11-26 05:24:15 -0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,6 +21,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
+   # await message.set_chat_protected_content(id, enabled)
     if not await present_user(id):
         try:
             await add_user(id)
@@ -88,13 +89,13 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("Post Channel", callback_data = "about"),
+                    InlineKeyboardButton("Backup Channel", url='https://telegram.me/')
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
+
+        await message.reply_photo( caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -102,9 +103,10 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            photo="https://i.ibb.co/pLLtXQK/Add-a-little-bit-of-body-text.jpg"
         )
+
+        
         return
 
     
@@ -120,6 +122,7 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
     
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
+   # await message.set_chat_protected_content(message.from_user.id, enabled)
     buttons = [
         [
             InlineKeyboardButton(
@@ -154,12 +157,14 @@ async def not_joined(client: Client, message: Message):
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
+    #await message.set_chat_protected_content(message.from_user.id, enabled)
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
+   # await message.set_chat_protected_content(message.from_user.id, enabled)
     if message.reply_to_message:
         query = await full_userbase()
         broadcast_msg = message.reply_to_message
